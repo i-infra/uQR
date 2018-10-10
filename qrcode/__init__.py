5d09ec912074e280b70c5b8f324318773262a4b6
Joseph Schilz <joseph@schilz.org>
2018-10-09 17:17:28 -0700

    Remove console scripts.

--- qrcode/__init__.py
+++ qrcode/__init__.py
@@ -1,5 +1,5 @@
 from qrcode.main import QRCode
-from qrcode.main import make  # noqa
+#from qrcode.main import make  # noqa
 from qrcode.constants import (  # noqa
     ERROR_CORRECT_L, ERROR_CORRECT_M, ERROR_CORRECT_Q, ERROR_CORRECT_H)
 
